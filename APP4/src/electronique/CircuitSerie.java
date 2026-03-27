787a400074e6e4d76bf7775081b351ecb075348a
Eric Burton-Labrecque <2501671@cegeplimoilou.ca>
2026-03-27 14:24:59 -0400

    Implémentation de calculerResistance() pour CircuitSerie

--- APP4/src/electronique/CircuitSerie.java
+++ APP4/src/electronique/CircuitSerie.java
@@ -9,6 +9,10 @@ public class CircuitSerie extends Circuit {
 
     @Override
     public double calculerResistance() {
-        return super.calculerResistance();
+        double resistance = 0;
+        for (Composant composant : composants) {
+            resistance += composant.calculerResistance();
+        }
+        return resistance;
     }
 }
